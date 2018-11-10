56dbaa670d0024fe1f8a01a88c20d03d5f0ca1d7
unknown <aleekanoo@gmail.com>
2018-11-10 13:05:27 -0300

    tp

---
+++ src/views/App.vue
@@ -0,0 +1,25 @@
+<style>
+#app {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 5;
+}
+</style>
+
+<template>
+  <main id="app" class="__full-height-perc __overflow-hidden">
+    <router-link to="/" class="__button __button-link __button-rounded __to-home">
+      <i class="fas fa-home"></i>
+    </router-link>
+    <router-view></router-view>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
