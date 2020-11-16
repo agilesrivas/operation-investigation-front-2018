bb14911967178660eeb6223b3e75ab3eefc16662
agilesrivas <alitoayrton@hotmail.com>
2020-11-15 22:23:10 -0300

    first commit proyect university

--- tp/src/main/java/com/example/tp/repositories/ConfigRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tp.repositories;
-
-import com.example.tp.models.Config;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConfigRepository extends JpaRepository<Config,Long> {
-}
--- tp/src/main/java/com/example/tp/repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.tp.repositories;
-
-
-import com.example.tp.models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long>{
-}
